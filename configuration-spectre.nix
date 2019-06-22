ea0ef86548904633967a91dc8d54c903d85d37e2
Imran Hossain <imran3740@gmail.com>
2019-06-21 22:30:51 -0400

    Track spectre configuration

---
+++ configuration-spectre.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Create swapfile
+  swapDevices = [
+    {device = "/var/swapfile"; size = 8192;}
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Optimize for SSDs
+  fileSystems."/".options = [ "discard" ];
+
+  # Define encrypted partition
+  boot.initrd.luks.devices."cryptroot" = {
+    preLVM = true;
+    allowDiscards = true;
+  };
+
+  networking.hostName = "Imran-SpectreNix"; # Define your hostname.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+  
+  # NVIDIA
+  nixpkgs.config.allowUnfree = true;
+  hardware.nvidiaOptimus.disable = true;
+  hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
+  hardware.opengl.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
+  # TODO get primerun script from https://nixos.wiki/wiki/Nvidia
+
+  # Set timezone
+  time.timeZone = "America/New_York";
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    wget neovim fish firefox git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # GDM + GNOME 3
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.imran = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+
+}
