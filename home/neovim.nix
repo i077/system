67c3ae6b364cd039e769848b0b167e31ed07aee0
Imran Hossain <imran3740@gmail.com>
2019-07-11 11:18:56 -0400

    nvim: Fix spacing in plugin decl.

--- home/neovim.nix
+++ home/neovim.nix
@@ -9,43 +9,43 @@ in
     configure = {
       plug.plugins = with pkgs.vimPlugins; [
         # Editor
-        sensible			# Sensible defaults
-        repeat				# Repeatable plugin actions
-        surround			# Surround text
-        commentary			# Comment code with 'gcc'
-        easy-align			# Alignment plugin
-        easymotion			# Enhanced motions
+        sensible                    # Sensible defaults
+        repeat	            		# Repeatable plugin actions
+        surround            		# Surround text
+        commentary        			# Comment code with 'gcc'
+        easy-align        			# Alignment plugin
+        easymotion        			# Enhanced motions
 
         # Extensions
-        fugitive			# Git wrapper
-        vim-dispatch			# Asynchronous job runner
-        fzf-vim				# Fuzzy entry selection
-        denite-nvim			# Async unite interface
-        nvim-yarp			# Remote plugin framework
-        nerdtree			# File sidebar
+        fugitive		            # Git wrapper
+        vim-dispatch                # Asynchronous job runner
+        fzf-vim			           	# Fuzzy entry selection
+        denite-nvim		           	# Async unite interface
+        nvim-yarp		           	# Remote plugin framework
+        nerdtree		           	# File sidebar
 
         # UI + Colorschemes
-        airline				# Smarter tabline
-        vim-airline-themes		# Airline themes (automatches colorscheme)
-        base16-vim			# Base16
-        falcon				# Falcon
-        gruvbox				# Gruvbox
-        awesome-vim-colorschemes	# Collection of colorschemes
+        airline	                    # Smarter tabline
+        vim-airline-themes          # Airline themes (automatches colorscheme)
+        base16-vim                  # Base16
+        falcon	                    # Falcon
+        gruvbox	                    # Gruvbox
+        awesome-vim-colorschemes    # Collection of colorschemes
 
         # Languages
-        ale				# Async linting framework
-        deoplete-nvim                   # Completion framework
-        LanguageClient-neovim		# Integrations with language clients
-        polyglot			# Multiple language support
-        vimtex				# LaTeX support
-        vim-pandoc			# Pandoc support
-        vim-pandoc-syntax		# Pandoc syntax support
-        vim-slime			# Scheme support
+        ale                         # Async linting framework
+        deoplete-nvim               # Completion framework
+        LanguageClient-neovim	    # Integrations with language clients
+        polyglot		            # Multiple language support
+        vimtex			            # LaTeX support
+        vim-pandoc                  # Pandoc support
+        vim-pandoc-syntax           # Pandoc syntax support
+        vim-slime		        	# Scheme support
 
         # Prose
-        goyo				# Distraction-free editing
-        limelight-vim			# Focus mode for vim
-        vim-pencil			# Writing tool for vim
+        goyo				        # Distraction-free editing
+        limelight-vim               # Focus mode for vim
+        vim-pencil			        # Writing tool for vim
       ];
       customRC = ''
         ${readVimSection "editor"}
