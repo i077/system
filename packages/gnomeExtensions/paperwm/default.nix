986415a2d257d10112ac929539f93944568ac901
Imran Hossain <imran3740@gmail.com>
2019-08-27 10:40:43 -0400

    packages: Update PaperWM

--- packages/gnomeExtensions/paperwm/default.nix
+++ packages/gnomeExtensions/paperwm/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "paperwm-${version}";
-  version = "2019-06-27-86beb27";
+  version = "2019-07-24";
 
   src = fetchFromGitHub {
     owner = "paperwm";
     repo = "PaperWM";
-    rev = "86beb27e04045ce38e4a0da7267ef600a2cfdcfc";
-    sha256 = "0daqh8qw1g1i3icf9p01f5x4ga6wqvlvyk3h6spvwpfxj4aagfsx";
+    rev = "611680e06be5ef4f1e9aa1da85de6b83ee5fba31";
+    sha256 = "0ql3zd66b83kzyidsr6rpfqincyljgb9j8k0p0c58s6fgsvwzhc4";
   };
 
   uuid = "paperwm@hedning:matrix.org";
