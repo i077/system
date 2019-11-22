f54c8f602816e329826146f6a561f533ea121866
Imran Hossain <imran3740@gmail.com>
2019-11-22 15:03:16 -0500

    Separate Keybase services

--- machines/spectre/services.nix
+++ machines/spectre/services.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
@@ -19,32 +16,4 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
   services.dbus.packages = with pkgs; [ gnome3.dconf ];
-
-  # Keybase
-  services.kbfs = {
-    enable = true;
-    mountPoint = "/keybase";
-    extraFlags = [ "-label kbfs" ];
-  };
-
-  systemd.user.services = {
-    keybase.serviceConfig.Slice = "keybase.slice";
-
-    kbfs = {
-      environment = { KEYBASE_RUN_MODE = "prod"; };
-      serviceConfig.Slice = "keybase.slice";
-    };
-
-    keybase-gui = {
-      description = "Keybase GUI";
-      requires = [ "keybase.service" "kbfs.service" ];
-      after    = [ "keybase.service" "kbfs.service" ];
-      serviceConfig = {
-        ExecStart  = "${pkgs.keybase-gui}/share/keybase/Keybase";
-        PrivateTmp = true;
-        Slice      = "keybase.slice";
-      };
-    };
-  };
-
 }
