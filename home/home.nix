be8d0554d72d4c49bd32bf46c112053dc99044c9
Imran Hossain <imran3740@gmail.com>
2019-12-21 17:17:39 -0500

    home: Add Iosevka Nerd Font

--- home/home.nix
+++ home/home.nix
@@ -5,6 +5,18 @@ let
     config = config.nixpkgs.config;
   };
   custompkgs = import ../packages {};
+  nerdfonts-iosevka = let version = "2.0.0"; in pkgs.fetchzip rec {
+    name = "nerdfonts-iosevka";
+
+    url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/Iosevka.zip";
+
+    sha256 = "1b8agwll5safqzxcf80sinxwhgqmrh3jh2arvskshvll29dzigp2";
+
+    postFetch = ''
+      mkdir -p $out/share/fonts
+      unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
+    '';
+  };
 in
 {
   imports = with builtins;
@@ -77,7 +89,7 @@ in
 
     # Fonts
     corefonts # Microsoft core fonts for Web
-    iosevka
+    iosevka nerdfonts-iosevka
     inter-ui
     roboto roboto-mono
     ubuntu_font_family
