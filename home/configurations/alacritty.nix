5c1998bf71f3943d529ee8e1722d6d15baa7441e
Imran Hossain <imran3740@gmail.com>
2019-12-23 14:36:58 -0500

    alacritty, fish: Use colors from colors.nix

--- home/configurations/alacritty.nix
+++ home/configurations/alacritty.nix
@@ -1,11 +1,37 @@
 { config, lib, pkgs, ... }:
 
-lib.mkIf config.programs.alacritty.enable {
+let
+  # Need to replace '#' in colors with '0x'
+  myColors = with lib;
+    mapAttrs (name: value: "0x" + (removePrefix "#" value)) (import ../colors.nix);
+in lib.mkIf config.programs.alacritty.enable {
   programs.alacritty.settings = {
-    window.decorations = "none";
     background_opacity = 0.8;
-    font.normal.family = "Iosevka";
-    font.bold.family = "Iosevka";
-    font.italic.family = "Iosevka";
+
+    font = {
+      normal.family = "Iosevka";
+      bold.family = "Iosevka";
+      italic.family = "Iosevka";
+
+      size = 12.0;
+    };
+
+    colors = with myColors; {
+      primary = {
+        background = bg0;
+        foreground = fg0;
+      };
+
+      normal = {
+        black = bg0;
+        red = alert;
+        green = secondary;
+        yellow = quinary;
+        blue = senary;
+        magenta = tertiary;
+        cyan = septary;
+        white = fg0;
+      };
+    };
   };
 }
