db0fde1033c9b3d55756c3d115d38b21341bc432
Imran Hossain <imran3740@gmail.com>
2020-02-03 01:26:37 -0500

    Update sources

--- overlays/unstable-fixes.nix
+++ overlays/unstable-fixes.nix
@@ -1,11 +1,7 @@
 self: super:
 
 let
-  nixpkgs-100012e = import
-    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/100012e55bc2a82fc680cba31a426ad38ead6fab.tar.gz") {};
-  
   trace = builtins.trace;
 in
 {
-  onedrive = nixpkgs-100012e.onedrive;
 }
