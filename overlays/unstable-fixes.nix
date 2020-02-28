2be26c02a9d71b299136bd3371377784d2708fc1
Imran Hossain <contact@imranhossa.in>
2020-02-28 01:47:34 -0500

    Update sources

--- overlays/unstable-fixes.nix
+++ overlays/unstable-fixes.nix
@@ -2,6 +2,9 @@ self: super:
 
 let
   trace = builtins.trace;
+  nixpkgs-91fb0f2 = import
+    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/91fb0f2e4710d6a2f5e5cc55197afcddbd490c33.tar.gz") {};
 in
 {
+  neovim-unwrapped = nixpkgs-91fb0f2.neovim-unwrapped;
 }
