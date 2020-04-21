59b284cdff8ea506dcc6fde372375106b1b34bc4
Imran Hossain <contact@imranhossa.in>
2020-04-21 17:26:32 -0400

    Update sources
    Also, add a convenience function in unstable overlay to quickly import old
    nixpkgs commits.

--- overlays/unstable-fixes.nix
+++ overlays/unstable-fixes.nix
@@ -2,10 +2,27 @@ self: super:
 
 let
   trace = builtins.trace;
-  nixpkgs-f5a072b = import
-    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f5a072bdf4e4efd362149721dcbd84ef28735069.tar.gz") {};
+  importNixpkgsRev = { rev }: import (builtins.fetchTarball {
+    name = "nixpkgs-src-" + rev;
+    url = "https://github.com/NixOS/nixpkgs/archive/" + rev + ".tar.gz";
+  }) { config = import ../nixpkgs-config.nix; overlays = []; };
+
+  nixpkgs-a2d9f58 = importNixpkgsRev {
+    rev = "a2d9f5843336b00398542448321a497309907d05";
+  };
+  nixpkgs-52f76b8 = importNixpkgsRev {
+    rev = "52f76b890722878b068c95e562ed7c4c97752885";
+  };
 in
 {
   # Building NVIDIA module is broken on 5.6 kernel, use 5.5 for now
   linuxPackages_latest = self.linuxPackages_5_5;
+
+  inherit (nixpkgs-a2d9f58) onedrive;
+
+  python3 = super.python3.override {
+    packageOverrides = _: _: {
+      inherit (nixpkgs-52f76b8.python3Packages) scipy scikitlearn;
+    };
+  };
 }
