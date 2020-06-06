ee2f272b495e6c468cd9ed195fb75261964bafe1
Imran Hossain <contact@imranhossa.in>
2020-06-06 15:49:04 -0400

    gnome3: Add dconf-editor

--- modules/env/gnome3.nix
+++ modules/env/gnome3.nix
@@ -5,16 +5,16 @@
   services.xserver.desktopManager.gnome3.enable = true;
   services.dbus.packages = [ pkgs.gnome3.dconf ];
 
-  # Extensions
-  environment.systemPackages = with pkgs.gnomeExtensions; [
-    appindicator
-    dash-to-dock
-    gsconnect
-    night-theme-switcher
-    paperwm
-  ];
+  # GNOME tools
+  environment.systemPackages = (with pkgs.gnome3; [ dconf-editor ]) ++
+    # Extensions
+    (with pkgs.gnomeExtensions; [
+      appindicator
+      dash-to-dock
+      gsconnect
+      night-theme-switcher
+      paperwm
+    ]);
 
-  home-manager.users.imran.home.packages = with pkgs; [
-    gnome3.gnome-tweaks
-  ];
+  home-manager.users.imran.home.packages = with pkgs; [ gnome3.gnome-tweaks ];
 }
