90c7f6c12a3af259e0e6dc7582329a97dbee5302
Imran Hossain <contact@imranhossa.in>
2020-06-14 18:32:51 -0400

    Add background selector module for GNOME

--- modules/env/gnome3.nix
+++ modules/env/gnome3.nix
@@ -21,4 +21,11 @@
   ];
 
   home-manager.users.imran.home.packages = with pkgs; [ gnome3.gnome-tweaks ];
+
+  # Background selector
+  home-manager.users.imran.services.gnome-background = {
+    enable = true;
+    script = "/home/imran/Pictures/walls/selector.py";
+    interval = "1h";
+  };
 }
