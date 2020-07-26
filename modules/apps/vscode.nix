0c7f42cdc4593bb963b46aa1c680a2ea3c8176ab
Imran Hossain <contact@imranhossa.in>
2020-07-26 11:30:15 -0400

    vscode: Use home-manager module, add user settings

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -1,8 +1,34 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  extensions = with pkgs.vscode-extensions; [ ms-python.python bbenoist.Nix ];
+{
+  home-manager.users.imran.programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [ ms-python.python bbenoist.Nix ];
 
-  vscode-with-extensions =
-    pkgs.vscode-with-extensions.override { vscodeExtensions = extensions; };
-in { home-manager.users.imran.home.packages = [ vscode-with-extensions ]; }
+    userSettings = {
+      # Updates are handled through nix
+      "update.channel" = "none";
+      "extensions.autoUpdate" = false;
+
+      # Font
+      "editor.fontLigatures" = true;
+      "editor.fontFamily" = builtins.concatStringsSep ", " [
+        "'${config.theming.fonts.mono.family}'"
+        "'monospace'"
+        "'Droid Sans Fallback'"
+      ];
+
+      # UI
+      "editor.lineNumbers" = "off";
+      "editor.minimap.enabled" = false;
+
+      # Python
+      "python.pythonPath" = "/etc/profiles/per-user/imran/bin/python3";
+      "python.formatting.provider" = "black";
+      "python.formatting.blackPath" = "${pkgs.python3Packages.black}/bin/black";
+      "python.defaultInterpreterPath" = "python3"; # Should pick up from nix-shell
+      "python.autoUpdateLanguageServer" = false;
+      "python.languageServer" = "Microsoft";
+    };
+  };
+}
