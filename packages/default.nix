21b115729c2c3b65b9a5f501d0a8bfb34e17b0d0
Imran Hossain <contact@imranhossa.in>
2020-07-27 19:27:43 -0400

    packages: Remove obsidian
    Obsidian is now available in nixpkgs.

--- packages/default.nix
+++ packages/default.nix
@@ -4,7 +4,6 @@
   nixpkgs.overlays = [
     (self: super: {
       everdo = self.callPackage ./pkgs/everdo.nix { };
-      obsidian = self.callPackage ./pkgs/obsidian.nix { };
       write_stylus = self.callPackage ./pkgs/write_stylus.nix { };
       nerdfonts-iosevka = self.callPackage ./pkgs/nerdfonts-iosevka.nix { };
 
