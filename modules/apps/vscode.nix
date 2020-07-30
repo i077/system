5281e069d153ceafcc5ba7e5f56c8f14aae6ccf2
Imran Hossain <contact@imranhossa.in>
2020-07-30 17:26:17 -0400

    vscode: Remove user settings

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -4,31 +4,5 @@
   home-manager.users.imran.programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [ ms-python.python bbenoist.Nix ];
-
-    userSettings = {
-      # Updates are handled through nix
-      "update.channel" = "none";
-      "extensions.autoUpdate" = false;
-
-      # Font
-      "editor.fontLigatures" = true;
-      "editor.fontFamily" = builtins.concatStringsSep ", " [
-        "'${config.theming.fonts.mono.family}'"
-        "'monospace'"
-        "'Droid Sans Fallback'"
-      ];
-
-      # UI
-      "editor.lineNumbers" = "off";
-      "editor.minimap.enabled" = false;
-
-      # Python
-      "python.pythonPath" = "/etc/profiles/per-user/imran/bin/python3";
-      "python.formatting.provider" = "black";
-      "python.formatting.blackPath" = "${pkgs.python3Packages.black}/bin/black";
-      "python.defaultInterpreterPath" = "python3"; # Should pick up from nix-shell
-      "python.autoUpdateLanguageServer" = false;
-      "python.languageServer" = "Microsoft";
-    };
   };
 }
