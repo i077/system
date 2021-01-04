b8b224125687b8bb3e416b875f1f7c4802542941
Imran Hossain <hi@imranh.xyz>
2021-01-04 15:16:03 -0500

    Migrate everdo keys to sops
    Secrets are copied in at activation-time, since they may not be
    available until then.

--- modules/desktop/apps/everdo.nix
+++ modules/desktop/apps/everdo.nix
@@ -7,12 +7,21 @@ in {
   options.modules.desktop.apps.everdo.enable = mkEnableOption "Everdo";
 
   config = mkIf cfg.enable {
+    # Declare secrets
+    sops.secrets.everdo-key.owner = config.user.name;
+    sops.secrets.everdo-encryptionkey.owner = config.user.name;
+    sops.secrets.everdo-jwt.owner = config.user.name;
+
     hm.home.packages = [ pkgs.everdo ];
 
-    hm.xdg.configFile = {
-      "everdo/everdo.key".text = config.private.everdo.key;
-      "everdo/encryption-key".text = config.private.everdo.encryptionKey;
-      "everdo/jwt".text = config.private.everdo.jwt;
+    hm.home.activation."everdoSecrets" = {
+      before = [ ];
+      after = [ "linkGeneration" ];
+      data = with config.sops.secrets; ''
+        cp ${everdo-key.path} $XDG_CONFIG_HOME/everdo/everdo.key
+        cp ${everdo-encryptionkey.path} $XDG_CONFIG_HOME/everdo/encryption-key
+        cp ${everdo-jwt.path} $XDG_CONFIG_HOME/everdo/jwt
+      '';
     };
   };
 }
--- modules/private.nix
+++ modules/private.nix
@@ -10,20 +10,6 @@ let
     };
 in {
   options.private = {
-    everdo = mkOption {
-      description = ''
-        Everdo configuration.
-        Attributes map to contents of files in $XDG_CONFIG_HOME/everdo.
-      '';
-      type = types.submodule {
-        options = {
-          key = mkPrivOption "Everdo product key";
-          encryptionKey = mkPrivOption "ESS passphrase";
-          jwt = mkPrivOption "ESS access token";
-        };
-      };
-    };
-
     u2fAuthFile = mkOption {
       description = "Path to file containing u2f keys";
       type = types.path;
