0c99d6a3e250545ea7343ea1184d461a7d96b5bf
Imran Hossain <hi@imranh.xyz>
2021-02-11 20:49:38 -0500

    devel/nix: Add script to pull nix-index db from Mic92's repo

--- modules/devel/nix.nix
+++ modules/devel/nix.nix
@@ -9,5 +9,18 @@ in {
   options.modules.devel.nix.enable = mkEnableOption "Nix development tools";
 
   config =
-    mkIf cfg.enable { hm.home.packages = with pkgs; [ nix-index manix niv nixfmt nix-diff ]; };
+    mkIf cfg.enable { hm.home.packages = with pkgs; [
+      manix niv nixfmt nix-diff
+
+      # Shell script to update nix-index database from Mic92's repo
+      (pkgs.writeShellScriptBin "nix-index-update" ''
+        tag=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --tags --sort='v:refname' \
+          https://github.com/Mic92/nix-index-database \
+          | awk 'END {match($2, /([^/]+)$/, m); print m[0]}')
+        curl -L "https://github.com/Mic92/nix-index-database/releases/download/$tag/files" -o $XDG_RUNTIME_DIR/files-$tag
+        mv $XDG_RUNTIME_DIR/files-$tag $HOME/.cache/nix-index/files
+      '')
+      nix-index
+    ];
+  };
 }
