2c21dd1fd3f533d37356f5975811c72a5ed6b4df
Imran Hossain <hi@imranh.xyz>
2021-02-16 10:18:51 -0500

    starship: Use nerd font symbols

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -10,7 +10,32 @@ in {
     hm.programs.starship = {
       enable = true;
       enableFishIntegration = config.modules.shell.fish.enable;
-      settings = { status.disabled = false; };
+      settings = {
+        status.disabled = false;
+        aws.symbol = " ";
+        conda.symbol = " ";
+        dart.symbol = " ";
+        directory.read_only = " ";
+        docker.symbol = " ";
+        elixir.symbol = " ";
+        elm.symbol = " ";
+        golang.symbol = " ";
+        haskell.symbol = " ";
+        hg_branch.symbol = " ";
+        java.symbol = " ";
+        julia.symbol = " ";
+        memory_usage.symbol = " ";
+        nim.symbol = " ";
+        nix_shell.symbol = " ";
+        nodejs.symbol = " ";
+        package.symbol = " ";
+        perl.symbol = " ";
+        php.symbol = " ";
+        python.symbol = " ";
+        ruby.symbol = " ";
+        rust.symbol = " ";
+        swift.symbol = "ﯣ ";
+      };
     };
   };
 }
