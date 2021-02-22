afc6c86f5b10c6e1cfd467fb343365d8d624f8f3
Imran Hossain <hi@imranh.xyz>
2021-02-22 13:12:45 -0500

    direnv: Store .direnv in $XDG_CACHE_HOME

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -12,6 +12,17 @@ in {
       enableNixDirenvIntegration = true;
 
       enableFishIntegration = config.modules.shell.fish.enable;
+
+      stdlib = ''
+        # Store .direnv in cache instead of project dir
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+                echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+                echo -n "$PWD" | shasum | cut -d ' ' -f 1
+            )}"
+        }
+      '';
     };
   };
 }
