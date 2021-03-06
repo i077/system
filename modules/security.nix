b398d3cd09175e1dc22500affadb25b7247ce35a
Imran Hossain <hi@imranh.xyz>
2021-03-06 14:50:14 -0500

    security: Require yubikey touch to authenticate

--- modules/security.nix
+++ modules/security.nix
@@ -12,29 +12,36 @@ in {
       boot.loader.systemd-boot.editor = false;
     }
     (mkIf cfg.yubikey.enable {
-      sops.secrets.authorized_yubikeys.owner = config.user.name;
+      sops.secrets = {
+        authorized_yubikeys.owner = config.user.name;
+        u2f_keys.owner = config.user.name;
+      };
 
-      security.pam.yubico = {
+      security.pam.u2f = {
         enable = true;
         control = "sufficient";
-        mode = "challenge-response";
+        cue = true;
       };
 
       security.pam.services = builtins.listToAttrs (map (name: {
         inherit name;
-        value = { yubicoAuth = true; };
+        value = { u2fAuth = true; };
       }) [ "login" "polkit-1" "su" "sudo" "systemd-user" "xlock" ]);
 
       # Add packages for yubikey management
       services.udev.packages = with pkgs; [ yubikey-personalization libu2f-host ];
 
-      # Place yubikey token in ~/.yubico/authorized_yubikeys
+      # Link yubikey info with right permissions
       hm.home.activation."yubikeysSecret" = {
         before = [ ];
         after = [ "linkGeneration" ];
         data = ''
           ln -sf ${config.sops.secrets.authorized_yubikeys.path} $HOME/.yubico/authorized_yubikeys
-          chmod 444 $HOME/.yubico/authorized_yubikeys
+          chmod 400 $HOME/.yubico/authorized_yubikeys
+
+          [ -d $XDG_CONFIG_HOME/Yubico ] || mkdir -p $XDG_CONFIG_HOME/Yubico
+          ln -sf ${config.sops.secrets.u2f_keys.path} $XDG_CONFIG_HOME/Yubico/u2f_keys
+          chmod 400 $XDG_CONFIG_HOME/Yubico/u2f_keys
         '';
       };
     })
