dc0e889aab5fd93a454eea8ecf9d3b1914c87993
Imran Hossain <hi@imranh.xyz>
2021-03-30 21:30:27 -0400

    git: Reorganize aliases, add some more

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -37,27 +37,64 @@ in {
         inherit (cfg) userName userEmail;
         signing.key = "211F871962EA405B";
 
-        aliases = {
-          ca = "commit --amend";
-          call = "commit -a";
+        aliases = let
+          # A nicer-looking one-line log format. Thanks @malob!
+          logformat =
+            "--format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %s %C(italic)- %an%C(reset)%C(magenta bold)%d%C(reset)'";
+          # Small function to get the default branch in origin
+          defaultBranch = remote:
+            ''!b() { git remote show ${remote} | grep "HEAD branch" | sed 's/.*: //' ;};'';
+        in {
+          # Staging/diffs
+          a = "add";
+          d = "diff";
+          dc = "diff --cached";
+          st = "status";
+
+          # Commit
+          c = "commit --verbose";
+          ca = "commit --amend --verbose";
+          call = "commit --all";
+          cm = "commit --message";
           credo = "commit --amend --no-edit";
-          credoall = "commit --amend --no-edit -a";
-          ci = "commit -v";
+          credoall = "commit --amend --no-edit --all";
+
+          # Checkout
           co = "checkout";
           cob = "checkout -b";
-          f = "fetch";
-          ignore = "!gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}; gi";
+          com = "${defaultBranch "origin"} git checkout $(b)";
+
+          # Log
           l = "log";
-          lg = "log --oneline --graph --decorate";
-          lga = "log --oneline --graph --decorate --all";
+          lg = "log --oneline --graph --decorate ${logformat}";
+          lga = "log --oneline --graph --decorate --all ${logformat}";
+
+          # Push/pull/fetch
+          f = "fetch";
           pd = "pull";
           pdu = "pull upstream";
-          pdum = "pull upstream master";
+          pdum = "${defaultBranch "upstream"} git pull upstream $(b)";
           pu = "push";
           puf = "push --force-with-lease"; # as in "poof" go your old commits!
-          pushall = "!git remote | xargs -L1 git push --all";
-          st = "status";
+          puu =
+            "head() { git rev-parse --abbrev-ref HEAD ;}; git push --set-upstream origin $(head)";
+
+          # Rebase
+          rb = "rebase";
+          rba = "rebase --abort";
+          rbc = "rebase --continue";
+          rbi = "rebase --interactive";
+
+          # Stash
+          sh = "stash";
+          sha = "stash apply";
+          shd = "stash drop";
+          shl = "stash list";
+          shp = "stash pop";
+          shs = "stash show --patch";
+
           whohas = "branch -a --contains";
+          ignore = "!gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}; gi";
         };
 
         extraConfig = {
@@ -97,7 +134,10 @@ in {
       hm.programs.gh = {
         enable = true;
 
-        aliases = { co = "pr checkout"; };
+        aliases = {
+          co = "pr checkout";
+          cl = "repo clone";
+        };
 
         gitProtocol = "ssh";
       };
