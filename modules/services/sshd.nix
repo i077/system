96ca4400c125ba51ba9fd91a20f7be9a574aff00
Imran Hossain <hi@imranh.xyz>
2021-04-06 12:44:56 -0400

    sshd: Actually block access by default
    0.0.0.0 accepts all attempts to connect. Oops! Removing the
    listenAddresses option for now, the default is the empty list anyway.

--- modules/services/sshd.nix
+++ modules/services/sshd.nix
@@ -12,11 +12,6 @@ in {
       services.openssh = {
         enable = true;
         passwordAuthentication = false;
-        # Block access by default
-        listenAddresses = mkDefault [{
-          addr = "0.0.0.0";
-          port = 22;
-        }];
       };
 
       programs.ssh.startAgent = true;
@@ -24,7 +19,6 @@ in {
       programs.mosh.enable = true;
     }
     (mkIf cfg.enable {
-      services.openssh.listenAddresses = [ ];
       # Add authorized keys for primary user
       user.openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDU6iRY3yHvhu1GeQg7AdLOsYEIpqNE2ik9u/kmHA+TmX6PsqtfGyNpiS+fEgTd8ZrFetFhUR5wmJazVw7Xjjt+K8JkXl1mUvXzbfat8AoONnr7f6cqH8+B022g7Nb3hh4/4mhCP+jIjZ9T22bIhzJGTaH18yf/L8zJK7baOpJv2jPpEyNbl4mj5AWXtYXq8tt2LHG1yiNT9HDHFHUx+XZZY2npIieiyuFNieClIkxzIyQw1j5M0D7lstz3Mqe5KyhoBZRVXSI4njCzl3YL0JQHbT3qD+9SAaq/nlWizaMM5056pdRrgqmEWyDGdrdgPzt0240NKLlxKRsplqUFeM7z imran@giratina"
