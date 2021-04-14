56bc81ee81a08bb2baa2f3c74c8c4812110d7ac3
Imran Hossain <hi@imranh.xyz>
2021-04-14 16:05:59 -0400

    Separate devShell to different file
    Flake.nix looks a little cleaner now.

--- flake.nix
+++ flake.nix
@@ -115,68 +115,6 @@
       overlay = final: prev: mkMyPkgs final;
 
       # Shell with dependencies for managing other outputs
-      devShell = forAllSystems (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ devshell.overlay ];
-          };
-          nixBin = pkgs.writeShellScriptBin "nix" ''
-            ${pkgs.nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
-          '';
-        in pkgs.devshell.mkShell {
-          name = "system-shell";
-
-          env = let nv = lib.nameValuePair;
-          in [
-            # Export PGP keys for sops
-            (nv "sopsPGPKeyDirs"
-              ''"$DEVSHELL_ROOT/secrets/pubkeys/hosts" "$DEVSHELL_ROOT/secrets/pubkeys/users"'')
-            {
-              name = "PATH";
-              prefix = "bin";
-            }
-          ];
-
-          packages = with pkgs; [
-            sops-nix.packages.${system}.sops-pgp-hook
-            fish
-            git
-            git-crypt
-            gnupg
-            nixBin
-            restic
-            utillinux
-
-            # Formatters used by treefmt
-            nixfmt
-            nodePackages.prettier
-          ];
-
-          commands = [
-            # Packages
-            { package = pkgs.sops; }
-            { package = pkgs.gitAndTools.gh; }
-            {
-              help = "Format the entire code tree";
-              category = "formatters";
-              package = treefmt.defaultPackage.${system};
-            }
-
-            # Custom commands
-            {
-              name = "update-pr";
-              help = "View the latest flakebot PR";
-              category = "maintenance";
-              command = "${pkgs.gitAndTools.gh}/bin/gh pr view -c flakebot";
-            }
-            {
-              name = "secrets";
-              help = "Edit secrets.yaml with sops";
-              category = "maintenance";
-              command = "${pkgs.sops}/bin/sops secrets/secrets.yaml";
-            }
-          ];
-        });
+      devShell = forAllSystems (import ./lib/devshell.nix inputs);
     };
 }
