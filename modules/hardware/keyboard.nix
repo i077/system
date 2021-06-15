91443d3843faadacd44c4a80944c620467e20457
Imran Hossain <hi@imranh.xyz>
2021-06-15 18:46:29 -0400

    keyboard: Use full paths in interception-tools config

--- modules/hardware/keyboard.nix
+++ modules/hardware/keyboard.nix
@@ -24,6 +24,12 @@ in {
       services.interception-tools = {
         enable = true;
         plugins = with pkgs.interception-tools-plugins; [ caps2esc ];
+        udevmonConfig = ''
+          - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+            DEVICE:
+              EVENTS:
+                EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+        '';
       };
     })
 
