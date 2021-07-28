efed9fa2d03e4e924c45d65b7985578e1c7fc21d
Imran Hossain <hi@imranh.xyz>
2021-07-28 18:08:06 -0400

    android: Move Android Studio to separate option

--- modules/devel/android.nix
+++ modules/devel/android.nix
@@ -4,17 +4,23 @@
 
 let
   cfg = config.modules.devel.android;
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf mkMerge;
 in {
-  options.modules.devel.android.enable = mkEnableOption "Android development tools";
-
-  config = mkIf cfg.enable {
-    # Android Studio + ADB
-    hm.home.packages = with pkgs; [ android-studio ];
-    programs.adb.enable = true;
-
-    # Grant user access to ADB
-    user.extraGroups = [ "adbusers" ];
-    services.udev.packages = with pkgs; [ android-udev-rules ];
+  options.modules.devel.android = {
+    enable = mkEnableOption "Android development tools";
+    studio.enable = mkEnableOption "Android Studio";
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      # Android Studio + ADB
+      programs.adb.enable = true;
+
+      # Grant user access to ADB
+      user.extraGroups = [ "adbusers" ];
+      services.udev.packages = with pkgs; [ android-udev-rules ];
+    }
+
+    (mkIf cfg.studio.enable { hm.home.packages = with pkgs; [ android-studio ]; })
+  ]);
 }
