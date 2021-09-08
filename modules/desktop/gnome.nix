043c8ce959a0d5892887f92575dca810aac99b5a
Imran Hossain <hi@imranh.xyz>
2021-09-08 17:11:16 -0400

    gnome: Move extensions to sessionPath, add some extensions

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -15,28 +15,31 @@ in {
         # Enable Wayland w/ NVIDIA graphics if necessary
         nvidiaWayland = elem config.modules.hardware.video.gpu [ "optimus" "nvidia" ];
       };
-      desktopManager.gnome.enable = true;
+      desktopManager.gnome = {
+        enable = true;
+
+        # GNOME tools
+        sessionPath = (with pkgs.gnome; [ dconf-editor gnome-tweaks ]) ++
+          # Extensions
+          (with pkgs.gnomeExtensions; [
+            appindicator
+            advanced-alttab-window-switcher
+            blur-me
+            dash-to-dock
+            gsconnect
+            night-theme-switcher
+            paperwm
+            vertical-overview
+          ]);
+      };
     };
 
     services.dbus.packages = [ pkgs.gnome.dconf ];
-
-    # GNOME tools
-    environment.systemPackages = (with pkgs.gnome; [ dconf-editor ]) ++
-      # Extensions
-      (with pkgs.gnomeExtensions; [
-        appindicator
-        dash-to-dock
-        gsconnect
-        night-theme-switcher
-        paperwm
-      ]);
 
     environment.gnome.excludePackages = with pkgs.gnome;
       [
         gnome-software # No need for this since we use declarative nix
       ];
-
-    hm.home.packages = with pkgs; [ gnome.gnome-tweaks ];
 
     # Enable gnome-settings-daemon udev rules
     services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
