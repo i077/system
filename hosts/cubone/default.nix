34160fd7ff104296073fb114f3bdc0ba20c70a84
Imran Hossain <hi@imranh.xyz>
2022-02-09 22:51:15 -0500

    cubone: Add blocklist to dnsmasq
    cubone now acts as an adblocker for my tailnet. Cool!

--- hosts/cubone/default.nix
+++ hosts/cubone/default.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 
-{
+let
+  mySSHKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsq7jgT0egpEZ4QpgaFHRRxrwk7vzWVvZE0w7Bhk9hK imran@NTC-MacBook"
+  ];
+
+  blocklistPath = "/var/lib/dnsmasq/blocklist";
+  blocklistUrl =
+    "https://github.com/notracking/hosts-blocklists/raw/master/dnsmasq/dnsmasq.blacklist.txt";
+in {
   imports = [ ./hardware-configuration.nix ];
 
   system.stateVersion = "20.09";
@@ -12,41 +20,40 @@
       enable = true;
       version = 4;
     };
-    
+
     # Raspi 4 kernel
     kernelPackages = pkgs.linuxPackages_rpi4;
   };
-  
+
   # Add 2GB swap
   swapDevices = [{
     device = "/var/swapfile";
     size = 2048;
   }];
-  
+
   # Networking -- I only use Ethernet, no wireless here
   networking.hostName = "cubone";
   networking.interfaces.eth0.useDHCP = true;
-  
+
   time.timeZone = "America/New_York";
 
+  # Passwordless accounts, auth is done via keypair
   nix.settings.trusted-users = [ "@wheel" ];
   security.sudo.wheelNeedsPassword = false;
-  
+
   # Use nix flakes for local flake evaluation
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
-  
+
   # Add user
   users.users.imran = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsq7jgT0egpEZ4QpgaFHRRxrwk7vzWVvZE0w7Bhk9hK imran@NTC-MacBook"
-    ];
+    openssh.authorizedKeys.keys = mySSHKeys;
   };
-  
+
   # OpenSSH configuration
   services.openssh = {
     enable = true;
@@ -54,17 +61,39 @@
     kbdInteractiveAuthentication = false;
     permitRootLogin = "no";
   };
-  
-  # Enable tailscale
+
   services.tailscale.enable = true;
-  
+
   # Pihole-like functionality with dnsmasq
   services.dnsmasq = {
     enable = true;
+
     # Use CloudFlare's DNS servers
-    servers = [ "1.1.1.1" "1.0.0.1" ];
+    servers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+    alwaysKeepRunning = true;
+
+    # Block ads & trackers with dnsmasq
+    extraConfig = ''
+      conf-file=${blocklistPath}
+    '';
   };
   # Open port 53 to accept DNS requests
   networking.firewall.allowedTCPPorts = [ 53 ];
   networking.firewall.allowedUDPPorts = [ 53 ];
+
+  # Ensure at least an empty blocklist exists before starting dnsmasq,
+  # otherwise it might fail to start
+  systemd.services.dnsmasq.preStart = ''
+    mkdir -p $(dirname ${blocklistPath})
+    touch ${blocklistPath}
+  '';
+
+  # Update the blocklist from notracking's repo every night
+  systemd.services.dnsmasq-update-blocklist = {
+    startAt = "*-*-* 03:00:00";
+    before = [ "dnsmasq.service" ];
+    script = ''
+      ${pkgs.curl}/bin/curl -sSL -o ${blocklistPath} ${blocklistUrl}
+    '';
+  };
 }
