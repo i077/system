53424fd2eb9235c45d20d7bb0613ed701301a86b
Imran Hossain <hi@imranh.org>
2022-03-25 17:24:42 -0400

    darwin: Add nixbuild.net as remote linux builder

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -15,6 +15,15 @@
   nix.trustedUsers = [ "@admin" ];
   users.nix.configureBuildUsers = true;
 
+  # Add remote builders
+  nix.distributedBuilds = true;
+  nix.buildMachines = [{
+    hostName = "eu.nixbuild.net";
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    maxJobs = 100;
+    supportedFeatures = [ "benchmark" "big-parallel" ];
+  }];
+
   services.nix-daemon.enable = true;
 
   environment.systemPackages = with pkgs; [ coreutils ];
