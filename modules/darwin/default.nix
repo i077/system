e912c61780066098d559a2a157b8b0f0f8c14aed
Imran Hossain <hi@imranh.org>
2022-04-05 14:00:59 -0400

    darwin: Configure nix options
    Add to registry & binary caches

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,28 +1,65 @@
-{ inputs, pkgs, ... }:
+{ inputs, lib, pkgs, ... }:
 
 {
   imports = [ ./pam.nix ./brew.nix ];
 
-  # Enable flakes
-  nix.extraOptions = ''
-    build-users-group = nixbld
-    auto-optimise-store = true
-    experimental-features = nix-command flakes
-    max-jobs = auto
-  '';
-
-  # Add administrators to trusted users
-  nix.trustedUsers = [ "@admin" ];
   users.nix.configureBuildUsers = true;
 
-  # Add remote builders
-  nix.distributedBuilds = true;
-  nix.buildMachines = [{
-    hostName = "eu.nixbuild.net";
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-    maxJobs = 100;
-    supportedFeatures = [ "benchmark" "big-parallel" ];
-  }];
+  # Link each input to /etc/sources
+  environment.etc =
+    (lib.mapAttrs' (name: value: (lib.nameValuePair "sources/${name}" { source = value; })) inputs);
+
+  nix = {
+    # Enable flakes
+    extraOptions = ''
+      build-users-group = nixbld
+      auto-optimise-store = true
+      experimental-features = nix-command flakes
+      max-jobs = auto
+    '';
+
+    # Add administrators to trusted users
+    trustedUsers = [ "@admin" ];
+
+    # Add remote builders
+    distributedBuilds = true;
+    buildMachines = [{
+      hostName = "eu.nixbuild.net";
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      maxJobs = 100;
+      supportedFeatures = [ "benchmark" "big-parallel" ];
+    }];
+
+    # Add inputs to registry & nix path
+    nixPath = lib.mkForce [
+      "nixpkgs=/etc/sources/nixpkgs"
+      "home-manager=/etc/sources/home-manager"
+      "darwin=/etc/sources/darwin"
+    ];
+    registry = {
+      nixpkgs = {
+        from = {
+          id = "nixpkgs";
+          type = "indirect";
+        };
+        flake = inputs.nixpkgs;
+      };
+      darwin = {
+        from = {
+          id = "darwin";
+          type = "indirect";
+        };
+        flake = inputs.darwin;
+      };
+    };
+
+    # Add other binary caches
+    binaryCaches = [ "https://i077.cachix.org" "https://nix-community.cachix.org/" ];
+    binaryCachePublicKeys = [
+      "i077.cachix.org-1:v28tOFUfUjtVXdPol5FfEO/6wC/VKWnHkD32/aMJJBk="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   programs.zsh.enable = true;
 
