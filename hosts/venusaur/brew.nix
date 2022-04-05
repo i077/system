8b184b082053513065bc157564b1ca956c37eadf
Imran Hossain <hi@imranh.org>
2022-04-05 13:38:33 -0400

    venusaur: Add a few more apps

--- hosts/venusaur/brew.nix
+++ hosts/venusaur/brew.nix
@@ -1,10 +1,11 @@
 { ... }: {
   homebrew = {
-    casks = [ "altserver" "discord" "steam" "skim" "the-unarchiver" "wireshark" "zoom" "zotero" ];
+    casks = [ "altserver" "discord" "mullvadvpn" "steam" "skim" "the-unarchiver" "wireshark" "zoom" "zotero" ];
 
     masApps = {
       "Microsoft Remote Desktop" = 1295203466;
       "Reeder 5" = 1529448980;
+      Todoist = 585829637;
     };
   };
 }
