4af7479fa22eb3568ec149cb2d928a554f8ca2fd
Imran Hossain <hi@imranh.org>
2022-06-22 15:09:38 -0400

    treefmt: Add options to lua-format

--- modules/home/neovim/plugins/nvim-treesitter.lua
+++ modules/home/neovim/plugins/nvim-treesitter.lua
@@ -1,21 +1,21 @@
 require'nvim-treesitter.configs'.setup {
-    highlight = {
-        enable = vim.g.vscode == nil,
-        disable = {"latex"} -- vimtex is good enough
-    },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm"
-        }
-    },
-    indent = {
-        enable = true,
-        disable = {"lua", "python"} -- Broken for these languages
+  highlight = {
+    enable = vim.g.vscode == nil,
+    disable = {"latex"} -- vimtex is good enough
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm"
     }
+  },
+  indent = {
+    enable = true,
+    disable = {"lua", "python"} -- Broken for these languages
+  }
 }
 
 vim.o.foldmethod = 'expr'
