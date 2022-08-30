42e42fa50c2cd365bbcd75c7cd5793ebc727959c
Imran Hossain <hi@imranh.org>
2022-08-29 22:00:46 -0400

    cubone: Remove pi-hole container & unbound resolver

--- hosts/cubone/default.nix
+++ hosts/cubone/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ./unbound.nix ./time-machine.nix ];
+  imports = [ ./hardware-configuration.nix ./time-machine.nix ];
 
   boot = {
     loader.grub.enable = false;
@@ -24,23 +24,6 @@
   # Networking -- I only use Ethernet, no wireless here
   networking.hostName = "cubone";
   networking.interfaces.eth0.useDHCP = true;
-
-  # Pihole
-  # Open ports 53 to accept DNS requests, 80 for pihole portal, 67 for DHCP
-  networking.firewall.allowedTCPPorts = [ 53 80 ];
-  networking.firewall.allowedUDPPorts = [ 53 67 ];
-  virtualisation.oci-containers.containers.pihole = {
-    autoStart = true;
-    image = "pihole/pihole:2022.04.3";
-    ports = [ "53:53/tcp" "53:53/udp" "80:80" ];
-    volumes = [ "/var/lib/pihole/:/etc/pihole/" "/var/lib/dnsmasq.d:/etc/dnsmasq.d/" ];
-    environment = {
-      TZ = "America/NewYork";
-      DNSSEC = "true";
-    };
-    extraOptions = [ "--cap-add=NET_ADMIN" "--net=host" ];
-    workdir = "/var/lib/pihole/";
-  };
 
   environment.systemPackages = with pkgs; [ dig nmap ];
 }
