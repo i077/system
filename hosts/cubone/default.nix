11d046751e9dd1de6354663ae63432155bc8d3fc
Imran Hossain <hi@imranh.org>
2022-08-29 21:08:04 -0400

    Move core server options into modules/nixos
    Preparing for a new server...

--- hosts/cubone/default.nix
+++ hosts/cubone/default.nix
@@ -1,19 +1,7 @@
 { pkgs, ... }:
 
-let
-  mySSHKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsq7jgT0egpEZ4QpgaFHRRxrwk7vzWVvZE0w7Bhk9hK imran@NTC-MacBook"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1YIyqTUGvH71i6MWCsYPVoijYLZWfapmuMSR4aGAh9 Shortcuts on Geodude"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxhrUwCg/DcNQfG8CwIMdJsHu0jZWI2BZV/T6ka5N imran@venusaur"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIGGsm2JWPKT+pUJHs7l7el4OJFxQZjkC3T/oxkLUlG9 Shortcuts on venusaur"
-  ];
-
-  blocklistPath = "/var/lib/dnsmasq/blocklist";
-  blocklistUrl = "https://dnsmasq.oisd.nl/";
-in {
+{
   imports = [ ./hardware-configuration.nix ./unbound.nix ./time-machine.nix ];
-
-  system.stateVersion = "20.09";
 
   boot = {
     loader.grub.enable = false;
@@ -36,36 +24,6 @@ in {
   # Networking -- I only use Ethernet, no wireless here
   networking.hostName = "cubone";
   networking.interfaces.eth0.useDHCP = true;
-
-  time.timeZone = "America/New_York";
-
-  # Passwordless accounts, auth is done via keypair
-  nix.settings.trusted-users = [ "@wheel" ];
-  security.sudo.wheelNeedsPassword = false;
-
-  # Use nix flakes for local flake evaluation
-  nix.package = pkgs.nixFlakes;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-
-  # Add user
-  users.users.imran = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = mySSHKeys;
-  };
-
-  # OpenSSH configuration
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    kbdInteractiveAuthentication = false;
-    permitRootLogin = "no";
-  };
-  programs.mosh.enable = true;
-
-  services.tailscale.enable = true;
 
   # Pihole
   # Open ports 53 to accept DNS requests, 80 for pihole portal, 67 for DHCP
---
+++ modules/nixos/default.nix
@@ -0,0 +1,43 @@
+# Default config for my servers
+{ pkgs, ... }:
+
+let
+  mySSHKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsq7jgT0egpEZ4QpgaFHRRxrwk7vzWVvZE0w7Bhk9hK imran@NTC-MacBook"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1YIyqTUGvH71i6MWCsYPVoijYLZWfapmuMSR4aGAh9 Shortcuts on Geodude"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxhrUwCg/DcNQfG8CwIMdJsHu0jZWI2BZV/T6ka5N imran@venusaur"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIGGsm2JWPKT+pUJHs7l7el4OJFxQZjkC3T/oxkLUlG9 Shortcuts on venusaur"
+  ];
+in {
+  system.stateVersion = "20.09";
+
+  time.timeZone = "America/New_York";
+
+  # Passwordless accounts, auth is done via keypair
+  nix.settings.trusted-users = [ "@wheel" ];
+  security.sudo.wheelNeedsPassword = false;
+
+  # Use nix flakes for local flake evaluation
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Add user
+  users.users.imran = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = mySSHKeys;
+  };
+
+  # OpenSSH configuration
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = "no";
+  };
+  programs.mosh.enable = true;
+
+  services.tailscale.enable = true;
+}
