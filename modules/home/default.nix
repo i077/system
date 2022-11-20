06eab03b929465e634dff5f07b1a7faa5221e729
Imran Hossain <hi@imranh.org>
2022-11-20 15:27:12 -0500

    home: Add rnix-lsp

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -8,7 +8,7 @@
       EDITOR = "nvim";
       HOMEBREW_NO_AUTO_UPDATE = 1;
     };
-    packages = with pkgs; [ curlie exa jq nix-index ripgrep ripgrep-all ];
+    packages = with pkgs; [ curlie exa jq nix-index ripgrep ripgrep-all rnix-lsp ];
 
     # Rebuild cache upon activation (for custom themes)
     activation."batCacheBuild" = {
