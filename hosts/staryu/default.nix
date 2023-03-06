2db43805c035ef93d7b79e34210594117b2466d8
Imran Hossain <hi@imranh.org>
2023-03-06 09:45:52 -0500

    staryu: Update keys for distributed builds

--- hosts/staryu/default.nix
+++ hosts/staryu/default.nix
@@ -18,7 +18,10 @@
 
   # Enable root login for distributed builds
   services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
-  users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTgmWqiXS1b+l8KhvdrjZtbXXCh5UuBnbnase5601p2"];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTgmWqiXS1b+l8KhvdrjZtbXXCh5UuBnbnase5601p2"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG13aSroi6VPpZII3u+0XkJyfE7ldbC6ovvMr3Fl6tMn"
+  ];
 
   # Allow cross-compilation of ARM builds
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
