a2d08839b738bd357983d5da1a34b23e254172df
Imran Hossain <hi@imranh.org>
2023-03-05 21:32:30 -0500

    staryu: Don't suspend on lid close

--- hosts/staryu/default.nix
+++ hosts/staryu/default.nix
@@ -1,10 +1,16 @@
 {inputs, ...}: {
   imports = [./hardware-configuration.nix inputs.nixos-hardware.nixosModules.microsoft-surface-common];
-  
+
   boot.loader.systemd-boot.enable = true;
 
   networking = {
     hostName = "staryu";
     networkmanager.enable = true;
   };
+
+  # Lock, but don't suspend, on lid close
+  # This is meant to run server workloads
+  services.logind.lidSwitch = "lock";
+  
+  # Enable distributed builds
 }
