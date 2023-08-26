fe45f9a97b26ce357aef8934f4fd9eb4f2192011
Imran Hossain <hi@imranh.org>
2023-08-26 16:55:51 -0400

    Replace staryu with nixbuild.net as linux builder
    Beginning to decomission staryu

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   nix.package = pkgs.nixUnstable;
 
   nix.settings = {
@@ -17,12 +12,14 @@
 
   # Add remote builders
   nix.distributedBuilds = true;
-  nix.buildMachines = lib.optional (config.networking.hostName != "staryu") {
-    hostName = "staryu";
-    systems = ["x86_64-linux" "aarch64-linux"];
-    maxJobs = 4;
-    supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-  };
+  nix.buildMachines = [
+    {
+      hostName = "eu.nixbuild.net";
+      systems = ["x86_64-linux" "aarch64-linux"];
+      maxJobs = 100;
+      supportedFeatures = ["benchmark" "big-parallel"];
+    }
+  ];
 
   # Use nix flakes for local flake evaluation
   nix.extraOptions = ''
