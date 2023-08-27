e71d134b1ff86a1d5b5c664595278c36f14e0c06
Imran Hossain <hi@imranh.org>
2023-08-27 17:58:40 -0400

    combee: Set data drive as noauto, systemd automount
    Don't want the boot to fail if the drive isn't present. Plex will still
    start up, but media will of course fail to play unless the drive is.
    Instead, systemd will mount the drive when it is present.

--- hosts/combee/plex.nix
+++ hosts/combee/plex.nix
@@ -3,6 +3,10 @@
   fileSystems."/mnt/data" = {
     device = "/dev/disk/by-uuid/62f089d4-627b-4812-8e58-f7ae47fc9f2b";
     fsType = "ext4";
+    options = [
+      "noauto" # Don't mount automatically at boot
+      "x-systemd.automount"
+    ];
   };
 
   # Expose drive as SMB share
