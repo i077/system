75a4de83f3285fcfbe66bf2a581e9317c6734122
Imran Hossain <hi@imranh.org>
2023-08-27 14:46:37 -0400

    Use combee as remote linux builder, remove nixbuild.net

--- modules/nix-distributed.nix
+++ modules/nix-distributed.nix
@@ -1,12 +1,14 @@
 {
+  config,
+  lib,
+  ...
+}: {
   # Add remote builders
   nix.distributedBuilds = true;
-  nix.buildMachines = [
-    {
-      hostName = "eu.nixbuild.net";
-      systems = ["x86_64-linux" "aarch64-linux"];
-      maxJobs = 100;
-      supportedFeatures = ["benchmark" "big-parallel"];
-    }
-  ];
+  nix.buildMachines = lib.optional (config.networking.hostName != "combee") {
+    hostName = "combee";
+    systems = ["x86_64-linux" "aarch64-linux"];
+    maxJobs = 4;
+    supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+  };
 }
