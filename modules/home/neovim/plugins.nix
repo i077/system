0e5ddc615ebd57800ea61f17c3a11d76fe42b93a
Imran Hossain <hi@imranh.org>
2023-09-13 18:34:18 -0400

    nvim: Add lualine, nvim-tree

--- modules/home/neovim/plugins.nix
+++ modules/home/neovim/plugins.nix
@@ -47,6 +47,69 @@ in {
         mapCW = true;
       };
 
+      # Focused writing
+      plugins.goyo = {
+        enable = true;
+        width = 120;
+      };
+
+      # Statusline
+      plugins.lualine = {
+        enable = true;
+        componentSeparators = {
+          left = "";
+          right = "";
+        };
+        sectionSeparators = {
+          left = "";
+          right = "";
+        };
+        iconsEnabled = false;
+
+        sections = {
+          # Only show the first char of the current mode
+          lualine_a = [
+            {
+              name = "mode";
+              extraConfig.fmt.__raw = "function(str) return str:sub(1,1) end";
+            }
+            "selectioncount"
+          ];
+          lualine_b = ["branch" "diff"];
+          lualine_c = [
+            {
+              name = "filename";
+              extraConfig.path = 1;
+            }
+          ];
+
+          lualine_x = ["diagnostics" "filetype"];
+          lualine_y = ["progress" "searchcount"];
+          lualine_z = ["location"];
+        };
+
+        tabline = {
+          lualine_a = [
+            {
+              name = "windows";
+              extraConfig.windows_color = {
+                active = "lualine_a_normal";
+                inactive = "lualine_b_inactive";
+              };
+            }
+          ];
+          lualine_z = [
+            {
+              name = "tabs";
+              extraConfig.tabs_color = {
+                active = "lualine_a_normal";
+                inactive = "lualine_b_inactive";
+              };
+            }
+          ];
+        };
+      };
+
       # Extra plugins that aren't yet in nixvim
       extraPlugins = with pkgs.vimPlugins; [
         sensible # Sensible defaults
@@ -55,11 +118,24 @@ in {
         direnv-vim # Direnv integration
       ];
     }
+
     # Visualize the undo tree
     {
       plugins.undotree.enable = true;
       maps.normal."<F5>" = ":UndotreeToggle<CR>";
     }
+
+    # File tree
+    {
+      plugins.nvim-tree.enable = true;
+
+      # <C-h> to open/close file tree
+      maps.normal."<C-h>" = {
+        silent = true;
+        action = ":NvimTreeToggle<CR>";
+      };
+    }
+
     # Git integration w/ vim-fugitive
     {
       plugins.fugitive.enable = true;
