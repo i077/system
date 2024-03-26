bf74aa641defebaff0dddb85bc2eae8065a0f444
Imran Hossain <hi@imranh.org>
2024-03-26 14:03:44 -0400

    Move XDG module to home-manager

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,11 +1,11 @@
 # Force all macOS utilities, e.g. zsh, to conform to the XDG Base Directory spec
-{config, ...}: let
-  inherit (config.home-manager.users.imran.lib.file) mkOutOfStoreSymlink;
-  homeDir = config.home-manager.users.imran.home.homeDirectory;
+{config, lib, ...}: let
+  inherit (lib.file) mkOutOfStoreSymlink;
+  homeDir = config.home.homeDirectory;
   # Use ~/Library for storing app files, I don't want any dotfiles cluttering up ~.
   # The exception is .config, where user-declared preferences are set (and the macOS analog,
   # ~/Library/Preferences, according to the Apple Developer docs, shouldn't have directories
-  # created by random apps.
+  # created by random apps).
   xdg = {
     cacheHome = "${homeDir}/Library/Caches";
     dataHome = "${homeDir}/Library/Application Support";
@@ -13,10 +13,8 @@
     stateHome = "${homeDir}/Library/Saved Application State";
   };
 in {
-  home-manager.users.imran = {
-    xdg = {
-      inherit (xdg) cacheHome dataHome stateHome;
-    };
-    home.sessionVariables.XDG_RUNTIME_DIR = xdg.runtimeDir;
+  xdg = {
+    inherit (xdg) cacheHome dataHome stateHome;
   };
+  home.sessionVariables.XDG_RUNTIME_DIR = xdg.runtimeDir;
 }
