7bdbbcbdd339f5df12777104085a06ea079e5dff
Imran Hossain <hi@imranh.org>
2024-03-26 14:03:47 -0400

    darwin: Don't set fish as login shell
    For POSIX-compatibility

--- hosts/workmac/default.nix
+++ hosts/workmac/default.nix
@@ -1,14 +1,10 @@
 {
-  config,
-  lib,
-  pkgs,
   ...
 }: {
   imports = [./brew.nix ./home.nix ../../modules/darwin/fish.nix ../../modules/darwin/1password.nix];
 
   # User config
   users.users.hossaini.home = "/Users/hossaini";
-  users.users.hossaini.shell = pkgs.fish;
 
   # Enable x86 builds
   nix.extraOptions = ''
