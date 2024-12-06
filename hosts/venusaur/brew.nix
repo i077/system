5d9869e095b754f21d2c6aa98c8be62ac3a588e8
Imran Hossain <hi@imranh.org>
2024-12-06 11:40:43 -0500

    darwin: Use standalone Tailscale

--- hosts/venusaur/brew.nix
+++ hosts/venusaur/brew.nix
@@ -12,6 +12,7 @@
       "skim"
       "sony-ps-remote-play"
       "swiftformat-for-xcode"
+      "tailscale"
       "the-unarchiver"
       "zoom"
       "zotero"
@@ -20,7 +21,6 @@
     masApps = {
       "Microsoft Remote Desktop" = 1295203466;
       "Reeder 5" = 1529448980;
-      Tailscale = 1475387142;
       Telegram = 747648890;
       "Things 3" = 904280696;
       WhatsApp = 310633997;
