08e176a193c366b169f6b9de1af70bed487bbef9
Imran Hossain <hi@imranh.org>
2025-09-09 11:29:21 -0400

    workmac: Update SSL

--- hosts/workmac/ssl.nix
+++ hosts/workmac/ssl.nix
@@ -9,7 +9,7 @@
     lib.optional (!config.lib.env.isCi)
     (pkgs.requireFile {
       name = "private-ca.cer";
-      hash = "sha256-VNdLlz0lEC7cQhEr+DXAHqkWWpPKtZDUjgI8vSSSmPU=";
+      hash = "sha256-JdOizsafGZhk4sASP7bUfXx/VcekINfO7Dp+dlnBuPQ=";
 
       message = ''
         Export the private root CA's certificate from Keychain Access, and add with
