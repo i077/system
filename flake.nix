53455059956bc1a08894be34199a980bb125cc24
Imran Hossain <hi@imranh.org>
2025-10-21 17:36:52 -0400

    Replace cirrus & GitHub actions w/ garnix

--- flake.nix
+++ flake.nix
@@ -32,12 +32,14 @@
     substituters = [
       "https://i077.cachix.org"
       "https://cache.nixos.org"
-      "https://nix-community.cachix.org/"
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "i077.cachix.org-1:v28tOFUfUjtVXdPol5FfEO/6wC/VKWnHkD32/aMJJBk="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
