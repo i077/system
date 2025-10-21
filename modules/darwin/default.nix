29126842419d7fc8d084e174d91d3aca686759cb
Imran Hossain <hi@imranh.org>
2025-10-21 17:36:52 -0400

    Replace cirrus & GitHub actions w/ garnix

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -31,10 +31,15 @@
       extra-platforms = ["x86_64-darwin" "aarch64-darwin"];
 
       # Add other binary caches
-      substituters = ["https://i077.cachix.org" "https://nix-community.cachix.org/"];
+      substituters = [
+        "https://i077.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cache.garnix.io"
+      ];
       trusted-public-keys = [
         "i077.cachix.org-1:v28tOFUfUjtVXdPol5FfEO/6wC/VKWnHkD32/aMJJBk="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
 
