c45a3b904246bfdcc91d8099b84fdc0a3edd0a03
Imran Hossain <hi@imranh.org>
2026-05-18 21:20:57 -0400

    paneru: Lessen dimming, use alt as modifier

--- modules/home/wm.nix
+++ modules/home/wm.nix
@@ -25,36 +25,36 @@
       swipe.gesture.fingers_count = 3;
 
       decorations.inactive.dim = {
-        opacity = -0.2;
-        opacity_night = -0.3;
+        opacity = -0.1;
+        opacity_night = -0.1;
       };
 
       bindings = {
-        window_focus_west = "ctrl+alt-h";
-        window_focus_east = "ctrl+alt-l";
-        window_focus_north = "ctrl+alt-k";
-        window_focus_south = "ctrl+alt-j";
+        window_focus_west = "alt-h";
+        window_focus_east = "alt-l";
+        window_focus_north = "alt-k";
+        window_focus_south = "alt-j";
 
-        window_swap_west = "ctrl+alt+shift-h";
-        window_swap_east = "ctrl+alt+shift-l";
-        window_swap_north = "ctrl+alt+shift-k";
-        window_swap_south = "ctrl+alt+shift-j";
+        window_swap_west = "alt+shift-h";
+        window_swap_east = "alt+shift-l";
+        window_swap_north = "alt+shift-k";
+        window_swap_south = "alt+shift-j";
 
-        window_virtual_north = "ctrl+alt-m";
-        window_virtual_south = "ctrl+alt-n";
-        window_virtualmove_north = "ctrl+alt+shift-m";
-        window_virtualmove_south = "ctrl+alt+shift-n";
-        window_nextdisplay = "ctrl+alt+shift-]";
+        window_virtual_north = "alt-m";
+        window_virtual_south = "alt-n";
+        window_virtualmove_north = "alt+shift-m";
+        window_virtualmove_south = "alt+shift-n";
+        window_nextdisplay = "alt+shift-]";
 
-        window_stack = "ctrl+alt-i";
-        window_unstack = "ctrl+alt-o";
+        window_stack = "alt-i";
+        window_unstack = "alt-o";
 
-        window_center = "ctrl+alt-c";
+        window_center = "alt-c";
 
-        window_manage = "ctrl+alt-f";
+        window_manage = "alt-f";
 
-        window_resize = "ctrl+alt-r";
-        window_fullwidth = "ctrl+alt-return";
+        window_resize = "alt-r";
+        window_fullwidth = "alt-return";
       };
 
       windows.antinote = {
