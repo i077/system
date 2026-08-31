288f874453a96579a4102ecb59e45073186ce6e0
Imran Hossain <hi@imranh.org>
2026-08-31 12:15:23 -0400

    venusaur: Add some apps

--- hosts/venusaur/darwin-configuration.nix
+++ hosts/venusaur/darwin-configuration.nix
@@ -46,14 +46,15 @@ in {
   homebrew.masApps = {
     "1Password for Safari" = 1569813296;
     "Bear" = 1091189122;
-    Current = 6758530974;
     "Kagi for Safari" = 1622835804;
     Sofa = 1276554886;
+    "StopTheMadness Pro" = 6471380298;
     "Things 3" = 904280696;
     Weathergraph = 1501958576;
     "Windows App" = 1295203466;
     WhatsApp = 310633997;
     Unread = 1363637349;
+    Unwatched = 6477287463;
     "uBlock Origin Lite" = 6745342698;
     Xcode = 497799835;
   };
